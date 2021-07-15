853bdb6edeb858ebfa0b4368cd1221ec727cb7b3
jonlazarini <jlazarini@deloitte.co.uk>
2021-07-15 15:31:52 +0100

    chore(cvsb-19465): format

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,14 @@
-import {atfGenInit} from "./functions/atfGenInit";
-import {config as AWSConfig} from "aws-sdk";
+import { atfGenInit } from "./functions/atfGenInit";
+import { config as AWSConfig } from "aws-sdk";
 
-const isOffline: boolean = (!process.env.BRANCH || process.env.BRANCH === "local");
+const isOffline: boolean =
+  !process.env.BRANCH || process.env.BRANCH === "local";
 
 if (isOffline) {
-    AWSConfig.credentials = {
-        accessKeyId: "offline",
-        secretAccessKey: "offline"
-    };
+  AWSConfig.credentials = {
+    accessKeyId: "offline",
+    secretAccessKey: "offline",
+  };
 }
 
-export {atfGenInit as handler};
+export { atfGenInit as handler };
