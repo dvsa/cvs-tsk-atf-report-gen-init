52ab06bbd67e3aed71b72ad2898111ff49482637
sul <sulaiman.aga@kainos.com>
2022-09-29 15:03:32 +0100

    chore(cda-446): Fixed sonar cube configs. Fixed files to match existing lint and prettier rules. Ignored typescript errors to get linting to work

--- tests/unit/atfGenInitFunction.unitTest.ts
+++ tests/unit/atfGenInitFunction.unitTest.ts
@@ -1,43 +1,43 @@
-import mockContext = require("aws-lambda-mock-context");
-import { SQService } from "../../src/services/SQService";
-import { StreamService } from "../../src/services/StreamService";
-import { atfGenInit } from "../../src/functions/atfGenInit";
+import mockContext = require('aws-lambda-mock-context');
+import { SQService } from '../../src/services/SQService';
+import { StreamService } from '../../src/services/StreamService';
+import { atfGenInit } from '../../src/functions/atfGenInit';
 
-describe("retroGenInit  Function", () => {
+describe('retroGenInit  Function', () => {
   const ctx = mockContext();
   afterAll(() => {
     jest.restoreAllMocks();
     jest.resetModuleRegistry();
   });
-  describe("with good event", () => {
-    it("should invoke SQS service with correct params", async () => {
-      const sendMessage = jest.fn().mockResolvedValue("Success");
+  describe('with good event', () => {
+    it('should invoke SQS service with correct params', async () => {
+      const sendMessage = jest.fn().mockResolvedValue('Success');
       SQService.prototype.sendMessage = sendMessage;
       StreamService.getVisitsStream = jest
         .fn()
-        .mockReturnValue([{ test: "thing" }]);
+        .mockReturnValue([{ test: 'thing' }]);
       await atfGenInit({}, ctx, () => {
-        return;
+
       });
       expect(sendMessage).toHaveBeenCalledWith(
-        JSON.stringify({ test: "thing" })
+        JSON.stringify({ test: 'thing' }),
       );
       expect(sendMessage).toHaveBeenCalledTimes(1);
     });
   });
 
-  describe("when SQService throws error", () => {
-    it("should bubble up the error", async () => {
+  describe('when SQService throws error', () => {
+    it('should bubble up the error', async () => {
       StreamService.getVisitsStream = jest.fn().mockReturnValue([{}]);
-      const myError = new Error("It Broke!");
+      const myError = new Error('It Broke!');
       SQService.prototype.sendMessage = jest.fn().mockRejectedValue(myError);
 
       expect.assertions(1);
       try {
-        const result = await atfGenInit({}, ctx, () => {
-          return;
+        await atfGenInit({}, ctx, () => {
         });
       } catch (e) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         expect(e.message).toEqual(myError.message);
       }
     });
